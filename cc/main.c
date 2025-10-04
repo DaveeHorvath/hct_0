71583709ab2ec8f634a2fd86a7b025c6318a4b88
dhorvath <dhorvath@student.hive.fi>
2025-10-04 03:35:01 -0400

    initial framework for execution

---
+++ cc/main.c
@@ -0,0 +1,96 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    const int EPOLL_SIZE = 20;
+    int polled;
+    struct epoll_event temp_event;
+    struct epoll_event* events;
+    int trackedFds = 3;
+    int eventAmount;
+
+    polled = epoll_create(EPOLL_SIZE);
+	if (polled == -1)
+	{
+		printf("FAILED POLL EXITING");
+        return 1;
+	}
+	events = (struct epoll_event*)calloc(EPOLL_SIZE * 2, sizeof(struct epoll_event));
+
+    int ircPipe[2];
+    pipe(ircPipe);
+    int authPipe[2];
+    pipe(authPipe);
+    int extPipe[2];
+    pipe(extPipe);
+    // start irc listener and write the output to one of the pipe-fds
+
+    pid_t ir = fork();
+    if (ir == 0)
+    {
+        printf("#### Started ir process with pid %d\n", ir);
+        dup2(1, ircPipe[0]);
+        
+        system("bash -c 'mode2 -d /dev/lirc1 | grep pulse'");
+    }
+    // start the python project and use it at a second filedescriptor
+    pid_t auth = fork();
+    if (auth == 0)
+    {
+        
+        printf("#### Started auth process with pid %d\n", ir);
+        dup2(1, authPipe[0]);
+        
+        system("python3 <evil-opencv-model-placeholder>");
+    }
+    // create site to get the recordings/to control the light remotely
+    pid_t ext = fork();
+    if (ext == 0)
+    {
+        
+        printf("#### Started external process with pid %d\n", ir);
+        
+        dup2(1, extPipe[0]);
+        system("# some random webserver");
+    }
+
+    if (epoll_ctl(polled, EPOLL_CTL_ADD, ircPipe[1], &temp_event) < 0)
+	    printf("Added irc output to poll");
+    if (epoll_ctl(polled, EPOLL_CTL_ADD, authPipe[1], &temp_event) < 0)
+	    printf("Added irc output to poll");
+    if (epoll_ctl(polled, EPOLL_CTL_ADD, extPipe[1], &temp_event) < 0)
+	    printf("Added irc output to poll");
+
+    // epoll the fds:
+        // upon input change the state: authenticated, connected to fd
+    while (1)
+    {
+        eventAmount = epoll_wait(polled, events, EPOLL_SIZE, 0); 
+        if (eventAmount == -1)
+        {
+            perror("");
+        }
+        for (int i = 0; i < eventAmount; i++)
+        {
+            // ir execution
+            if (events[i].data.fd == ircPipe[1])
+            {
+
+            }
+            // opencv execution
+            if (events[i].data.fd == authPipe[1])
+            {
+
+            }
+            // webserv execution
+            if (events[i].data.fd == extPipe[1])
+            {
+
+            }
+        }
+    }
+}
